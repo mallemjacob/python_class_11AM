76570d43bc21ad28f42a95714f1a86c2ae9585c1
jakejacobs <jacky4uonly2009@gmail.com>
2026-07-17 12:33:32 +0530

    Start functions, parameters and arguments.

---
+++ 16-july-2026.py
@@ -0,0 +1,65 @@
+# import random
+
+
+# my_random_number = random.randint(1, 10)
+
+
+# print(my_random_number)
+
+
+# import webbrowser
+
+# webbrowser.open('https://www.python.org/')
+# webbrowser.open('https://www.facebook.com')
+# webbrowser.open('https://www.instagram.com')
+
+
+# built-in functions
+# user-defined functions
+
+
+# functions
+# We can use functions for code reusability
+
+# print() - -> Shows output on screen
+
+# hello() - - -> "Hi there"
+
+# if conditon:
+#     code
+
+# while conditon:
+#     code
+
+
+# Function definiton
+
+
+# def hello(name):  # name ---> parameter, name = 'mouse'
+#     print("Hi there " + name)
+#     print("How are you doing " + name + "?")
+#     print("How old are you?")
+#     print('---------------------------')
+
+
+# # function Calling (Using function)
+# hello("mouse")  # --> Arguments
+# hello('cat')
+# hello('dog')
+# hello('elephant')
+
+
+# def adder(num1, num2):
+#     print("I am adding " + str(num1) + " and " + str(num2))
+#     total = num1 + num2
+#     print("The total is " + str(total))
+
+
+# adder(1, 2)
+
+
+# def adder(name, age):
+#     pass
+
+
+# adder(2, 'mouse')
